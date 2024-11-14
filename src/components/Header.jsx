9667fc777486f28b83a933cf0d72648d8102acf7
AritraKD <aritrakd19@gmail.com>
2024-11-14 16:55:46 +0530

    Create a basic Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-white shadow-md py-4">
+      <h1 className="text-center text-2xl font-bold">News App</h1>
+    </header>
+  );
+}
+
+export default Header;
