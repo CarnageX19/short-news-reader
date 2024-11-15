a0217920489117d0d4a00aa10e00da9f3c21811f
AritraKD <aritrakd19@gmail.com>
2024-11-15 20:58:25 +0530

    Added md classes for Viewer so that when any article is viewed in mobile devices, it overlaps the newspanel

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Viewer({ title, photo_url, description, content, url,onClose }) {
     
     return (
-        <div className="h-full p-6 relative overflow-y-auto">
+        <div className="fixed md:static 
+                top-0 right-0 
+                h-full md:h-auto 
+                w-full md:w-1/2 
+                bg-white shadow-lg z-50 md:z-30 
+                overflow-y-auto p-6">
             <button
                 className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
                 onClick={onClose}
